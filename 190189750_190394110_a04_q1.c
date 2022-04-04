08afe9896fae4aeb4899de3d99529bc8463e11e5
Jacob Cabral <jacobcabral@gmail.com>
2022-04-04 17:51:28 -0400

    added main and threadrun functions.

--- 190189750_190394110_a04_q1.c
+++ 190189750_190394110_a04_q1.c
@@ -38,6 +38,7 @@ typedef struct thread{
 	int retVal;
 } Thread;
 
+//This method reads the number of cusotmers from the file
 int read_customers(char *fname){
         
         FILE *file = fopen(fname,"r");
@@ -59,6 +60,7 @@ int read_customers(char *fname){
         return count;
 }
 
+//THis function counts the number of resources
 int count_resources(char *fname){
         
         int num_resource = 0;
@@ -80,6 +82,69 @@ int count_resources(char *fname){
 }
 
 
+//The The thread runner
+void *execute_thread(){
+        char ord[100];
+        int resources[cols];
+        int process[10];
+        int ind = 0;
+        int curr;
+        //show safe sequence
+        printf("Safe Seq: ");
+        fgets(ord, 100, stdin);
+        char *token = strtok(ord, " ");
+
+        while(token != NULL){
+                process[ind] = atoi(token);
+                token = strtok(NULL, " ");
+                ind += 1;
+
+        }
+
+        for (int i = 0; i < ind; i++){
+                printf("--> Thread %d\n", process[i]);
+                printf("Given: ");
+                for (int x = 0; x < cols; x++){
+                        curr =  allocation_ptr[process[i] * cols + x];
+                        printf("%d ", curr); 
+                        resources[x] = curr;
+                }
+                printf("\n");
+                printf("Need: ");
+                for (int r = 0; r < cols; r++){
+                        curr = need_ptr[process[i] * cols + r];
+                        printf("%d ", curr);
+                }
+                printf("\n");
+                printf("Available: ");
+                for (int r = 0; r < cols; r++){
+                        printf("%d ", available_ptr[r]);
+                }
+                printf("\n");
+                printf("Started...\n");
+                printf("Finished...\n");
+                printf("Releasing...\n");
+
+                for (int x = 0; x < cols; x++){
+                available_ptr[x] += resources[x];
+                *((available_ptr+ process[i] * cols) + x) -= resources[x];
+                *((need_ptr + process[i] * cols) + x) += resources[x]; 
+
+                }
+
+                printf("Available: ");
+                for (int r = 0; r < cols; r++){
+                printf("%d ", available_ptr[r]);
+                }
+                printf("\n");
+
+    }
+    return NULL;
+
+
+        
+}
+
 //Function to release resource
 int resource_release (int args[]){
 int i;
@@ -96,9 +161,10 @@ int i;
         }
         if (valid == true){
             for (i = 0; i < cols; i++){
+                    //clearing the resources
                 available_ptr[i] += resources[i];
                 *((allocation_ptr + cust_num * cols) + i) -= resources[i];
-                *((need_ptr + cust_num * cols) + i) += resources[i]; //Resources get released and added back to available
+                *((need_ptr + cust_num * cols) + i) += resources[i]; 
             }
             return 1; // resource released
 }
@@ -109,6 +175,7 @@ int i;
     return 0;
 }
 
+//Function to request resources
 int resource_request (int args[]){
     int customer_num = args[0];
     int req[cols];  
@@ -121,12 +188,12 @@ int resource_request (int args[]){
     int i;
     for (i = 0; i < cols && valid; i++)
     {
-        valid = req[i] <= *(need_ptr + (customer_num * cols + i)); // request[i] <= need[customer_num][i]
+        valid = req[i] <= *(need_ptr + (customer_num * cols + i)); 
     }
     if (valid == true){
         for (i = 0; i < cols && valid; i++)
         {
-            valid = req[i] <= *(available_ptr + i); // request[i <= available[i]
+            valid = req[i] <= *(available_ptr + i);
         }
         if (valid == true){
             for (i = 0; i < cols; i++){
@@ -143,12 +210,12 @@ int resource_request (int args[]){
                     *((allocation_ptr + customer_num * cols) + i) -= req[i];
                     *((need_ptr + customer_num * cols) + i) += req[i];
                 }
-                return 0; // not satsified
+                return 0; 
             }
         }
         else
         {
-            return 0; // not satsified
+            return 0; // 0 = not satisified
         }
     }
     else
@@ -157,42 +224,35 @@ int resource_request (int args[]){
     }
 }
 
-bool safety(int *available, int *allocated, int *need)
-{
+//the saftey algorithm
+bool safety(int *available, int *allocated, int *need){
     int work[cols];
-    for (int i = 0; i < cols; i++) 
-    {
+    for (int i = 0; i < cols; i++) {
         work[i] = *(available + i); 
 
     }
     printf("\n");
 
     bool finish[cols];
-    for (int i = 0; i < cols; i++)
-    {
+    for (int i = 0; i < cols; i++){
         finish[i] = false;
     }
 
     int safe_seq[rows];
-
+//begin checking
     int ind = 0;
-    while (ind < rows)
-    {
+    while (ind < rows){
         bool found = false;
-        for (int i = 0; i < rows; i++)
-        {
-            if (finish[i] == false)
-            {
+        for (int i = 0; i < rows; i++){
+            if (finish[i] == false){
                 int j = 0;
-                for (j = 0; j < cols; j++)
-                {
-                    if (*((need + i * cols) + j) > work[j]) // need[i][j] > allocated[i][j] (why do I need to do this, I hate C)
+                for (j = 0; j < cols; j++){
+                    if (*((need + i * cols) + j) > work[j]) 
                     {
                         break;
                     }
                 }
-                if (j == cols)
-                {
+                if (j == cols){
                     for (int y = 0; y < cols; y++)
                     {
                         work[y] += *((allocated + i * cols) + y);
@@ -204,8 +264,190 @@ bool safety(int *available, int *allocated, int *need)
                 }
             }
         }
-        if (found == false)
-        {
-            return false;
+        if (found == false){
+            return false; // return false if the system is not safe
+         }
         }
-    }
+}
+
+//main method
+int main(int argc, char *argv[]){
+        //we begin by getting the number of resources and customers on file. This helps to fill the matrix
+        cols = count_resources("sample4_in.txt"); 
+        rows = read_customers("sample4_in.txt");
+        int max[rows][cols];
+        int allocation[rows][cols];
+        int need[rows][cols];
+        //error checking
+        if(argc > cols + 1){
+                printf("excess amount of command line args\n");
+                return 0;
+        }
+        if(argc < cols + 1){
+                printf("lack of command line args\n");
+                return 0;
+        }
+        for (int i = 1; i < argc; i++)
+        {
+            if (isalpha(*argv[i]) != 0)
+            {
+                printf("non-digit input detected\n");
+                return 0;
+            }
+        }
+
+        //we now need to set the matricies
+        printf("Number of Customers: %d\n", rows);
+       
+        printf("Cuurently Available resources: ");
+        for (int i = 1; i < cols; i++){
+                printf("%s ", argv[i]);
+        }
+        printf("%s\n", argv[cols]);
+
+
+        
+        printf("Maximum resources from file:\n");
+        //these lines will open and set the MAX matrix
+        FILE *file = fopen("sample4_in.txt", "r");
+        char *token;
+        int x = 0;
+        while(!feof(file))
+	{
+		char line[100];
+                
+		if(fgets(line,100,file)!=NULL)
+		{
+			printf("%s",line);
+                        token = strtok(line, ",");
+                        int y = 0;
+                        while(token != NULL){
+                                max[x][y] = atoi(token);
+                                token = strtok(NULL,",");
+                                y += 1;
+                        }
+                                
+		}
+                x += 1;
+	}
+        fclose(file);
+        printf("\n");
+        
+
+        //Sets the available matrix
+        int *available = (int *)malloc(cols * sizeof(int)); 
+                for (int i = 0; i < cols; i++){
+                        available[i] = atoi(argv[i + 1]);
+                }
+        
+        
+        //Sets the allocation matrix
+        for (int x = 0; x < rows; x++){
+                for (int y = 0; y < cols; y++)
+                {
+                    allocation[x][y] = 0;
+                    
+                }
+        }
+
+
+        for (int x = 0; x < rows; x++){
+                for (int y = 0; y < cols; y++){
+                        need[x][y] = max[x][y] - allocation[x][y];
+                }
+                
+        }
+        
+        //inits
+        allocation_ptr = &available[0];
+        max_ptr = &max[0][0];
+        allocation_ptr = &allocation[0][0];
+        need_ptr = &need[0][0];
+
+        char cmd[100];
+    
+        bool running = true;
+        //main user control loop
+        while (running){
+                printf("Command: ");
+
+                fgets(cmd, 100, stdin);
+
+                char *token = strtok(cmd, " "); 
+                //clean string
+
+                int args[cols + 1];
+                token = strtok(NULL, " ");
+                int j = 0;
+                while (token != NULL){
+			args[j] = atoi(token);
+			token = strtok(NULL, " ");
+			j += 1;
+                }
+
+                for (char *cmd_lower = cmd; *cmd_lower; cmd_lower++){
+                        *cmd_lower = tolower(*cmd_lower);
+                }
+
+                //STATUS command
+                //This command will display the process' status
+                if(strstr(cmd,"status")){
+                        int current;
+                        printf("Available resources: \n");
+                        for (int i = 0; i < cols; i++){
+                                current = available_ptr[i];
+                                printf("%d ", current);
+                        }
+                        printf("\n");
+
+                        printf("Max Resources:\n");
+                        for (int i = 0; i < rows; i++){
+                                for (int x = 0; x < cols; x++){
+                                        current =  *((max_ptr + i * cols) + x);
+                                        printf("%d ", current);
+                                }
+                                printf("\n");
+                        }
+                        printf("Resources Allocated:\n");
+                        for (int i = 0; i < rows; i++){
+                                for (int x = 0; x < cols; x++){
+                                        current = *((allocation_ptr + i * cols) + x);
+                                        printf("%d ", current);
+                                }
+                                printf("\n");
+                        }
+                        printf("This Processes needs:\n");
+                        for (int i = 0; i < rows; i++){
+                                for (int x = 0; x < cols; x++){
+                                        current = *((need_ptr + i * cols) + x);
+                                        printf("%d ", current);
+                                }
+                                printf("\n");
+                        }
+                }
+
+                
+
+                else if (strstr(cmd, "rq") != NULL){
+                        valid_resc_request(args);               
+                }
+                else if (strstr(cmd, "run") != NULL){
+                        execute_thread();
+                }
+                else if (strstr(cmd, "rl") != NULL)
+                {
+                        printf("Releasing Resources-\n");
+                        resc_release(args);
+                }
+                
+                else if (strstr(cmd, "exit") != NULL){
+                        printf("Exiting program\n");
+                        running= false;
+                }
+                else{
+                        printf("INVALID INPUT\n");
+                }
+        }
+ 
+        
+}
